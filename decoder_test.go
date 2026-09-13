cd3b3af8248b79a02d2c5f76db8cb153177492ed
kiabq <conway.joseph@pm.me>
2026-09-12 23:35:20 -0400

    chore: add test cases for ParseInt

--- decoder_test.go
+++ decoder_test.go
@@ -1,65 +1,50 @@
 package main
 
 import (
-	"errors"
-	"math"
-	"strconv"
+	"fmt"
 	"testing"
 )
 
-//func TestParseType(t *testing.T) {
-//	tests := []struct {
-//		name     string
-//		val      []byte
-//		expected BencodeType
-//	}{
-//		{name: "Positive Integer", val: []byte("i42e"), expected: EncodedInt},
-//		{name: "Zero", val: []byte("i0e"), expected: EncodedInt},
-//		{name: "Byte String", val: []byte("4:spam"), expected: EncodedByteString},
-//		{name: "Byte String", val: []byte("5:thing"), expected: EncodedByteString},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			v, err := ParseType(tt.val)
-//
-//			if err != nil {
-//				t.Errorf("ParseString(%v) = %v\n", tt.val, v)
-//			}
-//		})
-//	}
-//}
-
 func TestDecoder(t *testing.T) {
-	d := Decoder{
-		buf: make([]byte, 0),
-		pos: 0,
-	}
-
-	d.buf = append(d.buf, []byte("l4:spam4:eggse")...)
-
-	_, err := d.ParseList()
-	if err != nil {
-		t.Errorf("ParseByteString: %v\n", err)
-	}
-}
-
-func TestInt64MaxValue(t *testing.T) {
-	//var maxInt64 string = "9223372036854775807"
-	var overflowInt64 = strconv.Itoa(math.MaxInt - 1)
-	_, err := strconv.Atoi("9223372036854775808")
-	if err != nil {
-		if errors.Is(err, strconv.ErrRange) {
-			t.Errorf("overflowString -> int64 exceeds range")
-		} else {
-			t.Errorf("unknown error: %v\n", err)
+	t.Run("ParseInt", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			val      []byte
+			pos      int
+			fail     bool
+			expected any
+		}{
+			{name: "positive integer", val: []byte("i1337e"), fail: false},
+			{name: "zero integer", val: []byte("i0e"), fail: false},
+			{name: "in a list", val: []byte("i42e"), pos: 3, fail: false},
+			{name: "leading zero integer", val: []byte("i042e"), fail: true},
+			{name: "max int overflow", val: []byte("i9223372036854775808e"), fail: true},
+			{name: "contains non-digit characters", val: []byte("i33892d21e"), fail: true},
+			{name: "negative zero", val: []byte("i-0e"), fail: true},
+			{name: "double negative", val: []byte("i--2e"), fail: true},
+			{name: "contains minus", val: []byte("i4-2e"), fail: true},
+			{name: "missing terminator character", val: []byte("i21321312"), fail: true},
 		}
-	}
 
-	_, err = strconv.ParseInt(overflowInt64, 10, 64)
-	if err != nil {
-		if errors.Is(err, strconv.ErrRange) {
-			t.Errorf("int64 exceeds range")
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := Decoder{
+					buf: make([]byte, tt.pos),
+					pos: tt.pos,
+				}
+
+				d.buf = append(d.buf, tt.val...)
+				integer, err := d.ParseInt()
+				fmt.Println("position: ", d.pos)
+				fmt.Println("buffer @ position: ", string(d.buf[:d.pos]))
+				fmt.Println("integer: ", integer)
+				fmt.Println("error: ", err)
+				if err != nil && !tt.fail {
+					t.Errorf("ParseInt: %v\n", err)
+				} else {
+					t.Logf("ParseInt: %d\n", integer)
+				}
+			})
 		}
-	}
+	})
 }
